0cb0afb925138b9800e2787f23c59c536e48178f
gabriel <gabrielmedinatorres@hotmail.com>
2024-11-29 13:14:48 +0000

    AutoPush Gabriel: 29-11-2024 13:13:28

--- hoocksPersonalizados/src/App.tsx
+++ hoocksPersonalizados/src/App.tsx
@@ -2,12 +2,12 @@ import UseCounter from "./Components/UseCounter";
 import UserList from "./Components/UserList";
 import WindowSizeApp from "./Components/WindowSizeApp";
 import "./App.css";
-import  TimeCounter from "./Components/TimeCounter";
+import TimeCounter from "./Components/TimeCounter";
 
 export default function App() {
   return (
     <div>
-      h1
+      <h1>Hoocks personalizados</h1>
       <UseCounter />
       <UserList />
       <WindowSizeApp />
