21718caa77a03b616c342268f0ff513a7bc8d7bf
gabriel <gabrielmedinatorres@hotmail.com>
2024-11-29 13:18:24 +0000

    AutoPush Gabriel: 29-11-2024 13:13:28

--- hoocksPersonalizados/src/Components/TimeCounter.tsx
+++ hoocksPersonalizados/src/Components/TimeCounter.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Countertimer } from "../Hooks";
 
 export default function TimeCounter() {
-  const countTimer = useCounter(0, 1000); // Inicializa el contador en 0 con un intervalo de 1 segundo.
+  const countTimer = Countertimer(0, 1000); // Usa el nombre correcto del hook.
 
   return <h1>Segundos transcurridos: {countTimer}</h1>;
 }
